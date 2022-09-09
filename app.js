edbdbab76494f05f7fa61f894abd81a1a4ad304d
mahirminhajk <kmahirminhaj@gmail.com>
2022-09-09 17:36:17 +0530

    project initialisation

---
+++ app.js
@@ -0,0 +1,67 @@
+require('dotenv').config();//.env
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');//encryption
+
+const app = express()
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+
+const port = 3000;
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+//                                          .env */
+userSchema.plugin(encrypt, { secret: process.env.SECRET_ENCRYPT, encryptedFields: ['password'] });//encryption
+const User = mongoose.model('User', userSchema);
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save((err) => {
+        if (err) console.log(err);
+        else res.render('secrets');
+    });
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({ email: username }, (err, foundUser) => {
+        if (!err) {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render('secrets');
+                } else {
+                    console.log("wrong password");
+                    res.redirect('/login');
+                }
+            } else {
+                console.log("user not found");
+                res.redirect('/login');
+            }
+        } else console.log(err);
+    })
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
