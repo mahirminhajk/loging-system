56f6de1119f7406af63ad37c13bfa980ed39a451
mahirminhajk <kmahirminhaj@gmail.com>
2022-09-15 23:54:30 +0530

    notes taken & rm all previous code

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-//const encrypt = require('mongoose-encryption');
-//const md5 = require('md5');//md5
 const bcrypt = require('bcrypt');
 
 
@@ -20,8 +18,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-//                                          .env */
-//userSchema.plugin(encrypt, { secret: process.env.SECRET_ENCRYPT, encryptedFields: ['password'] });
 const User = mongoose.model('User', userSchema);
 
 app.get('/', (req, res) => {
@@ -38,13 +34,14 @@ app.get('/register', (req, res) => {
 
 app.post('/register', (req, res) => {
 
-    //bcrypt
     bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-        const newUser = new User({
-            email: req.body.username,
-            //password: md5(req.body.password)
-            password: hash
-        });
+        if (!err) {
+            const newUser = new User({
+                email: req.body.username,
+                password: hash
+            });
+        } else console.log(err);
+
         newUser.save((err) => {
             if (err) console.log(err);
             else res.render('secrets');
@@ -54,12 +51,10 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
     const username = req.body.username;
-    //const password = md5(req.body.password);
     const password = req.body.password;
     User.findOne({ email: username }, (err, foundUser) => {
         if (!err) {
             if (foundUser) {
-                //bcrypt
                 bcrypt.compare(password, foundUser.password, (err, result) => {
                     if (!err) {
                         if (result) {
@@ -70,12 +65,6 @@ app.post('/login', (req, res) => {
                         }
                     } else console.log(err);
                 })
-                // if (foundUser.password === password) {
-                //     res.render('secrets');
-                // } else {
-                //     console.log("wrong password");
-                //     res.redirect('/login');
-                // }
             } else {
                 console.log("user not found");
                 res.redirect('/login');
