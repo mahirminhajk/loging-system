8342113bba2beeae8b483a0259d2c3ab1b006c6e
mahirminhajk <kmahirminhaj@gmail.com>
2022-09-17 14:26:47 +0530

    added header
    added passport & other package header code

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-require('dotenv').config();//.env
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const mongoose = require('mongoose');
+require('dotenv').config() //.env
+const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+const session = require('express-session') //passport
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
+const { use } = require('passport')
 
 const app = express()
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+//passport
+app.use(session({
+    secret: "My secert is not your secert.",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 mongoose.connect('mongodb://127.0.0.1:27017/userDB');
 
 const port = 3000;
@@ -15,7 +27,12 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
+userSchema.plugin(passportLocalMongoose)
 const User = mongoose.model('User', userSchema);
+passport.use(User.createStrategy())
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 
 app.get('/', (req, res) => {
     res.render('home');
